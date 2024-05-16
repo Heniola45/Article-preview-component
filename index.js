3eb06b4187f5f9fff09d5333fc5ff3140408ea55
Abdulazeez hauwa <abdulazeezhauwa1997@gmail.com>
2024-05-16 11:02:08 +0100

    an article preview component built with html, css and javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+function showLink() {
+    // document.getElementById("share_btn").className = "share-btn";
+    document.getElementById("social-link").className = "social-links";
+    document.getElementById("share").className = "share-change";
+    document.getElementById("triangle").className = "triangle";
+   
+
+}
+
+function hideLink(){
+    document.getElementById("social-link").className = "social-links-hidden";
+    document.getElementById("share").className = "share";
+    document.getElementById("triangle").className = "triangle-hidden";
+    
+
+}
